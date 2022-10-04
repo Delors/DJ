0b24ecabe16889c5955d1ef84739ad38994d4b5c
Michael Eichberg <michael.eichberg@bka.de>
2022-10-04 10:43:09 +0200

    added module to detriplicate entries

---
+++ operations/detriplicate.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from operations.operation import Operation
+
+
+class Detriplicate(Operation):
+    # If an entry is a triplication of a sequence, the first part
+    # will be returned. E.g. "TestTestTest" will be transformed to 
+    # "Test"
+   
+    def is_extractor(self) -> bool: 
+        return True
+
+    def process(self, entry: str) -> List[str]:
+        length = len(entry)
+        if  length % 3 != 0:
+            return None
+        frag_length = length//3
+        first_third = entry[0:frag_length]    
+        second_third = entry[frag_length:2*frag_length]    
+        third_third = entry[-frag_length:]    
+        if first_third == second_third and second_third == third_third:
+            return [first_third]
+        else:
+            return None
+
+    def __str__(self):
+        return "detriplicate"
+
+DETRIPLICATE = Detriplicate() 
