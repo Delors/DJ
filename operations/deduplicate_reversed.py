da16e41bfee3df6f60a21197ea43a8c8cfb04d52
Michael Eichberg <michael.eichberg@bka.bund.de>
2022-10-25 09:19:53 +0200

    added deduplicate_reversed as an operation

---
+++ operations/deduplicate_reversed.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from operations.operation import Operation
+
+
+class DeduplicateReversed(Operation):
+    # Where the second part of an entry is the duplication of the
+    # first part, but reversed. E.g., "testtset"
+   
+    def is_extractor(self) -> bool: 
+        return True
+
+    def process(self, entry: str) -> List[str]:
+        length = len(entry)
+        if  length % 2 == 1:
+            return None
+
+        first_half = entry[0:length//2]    
+        second_half = entry[-length//2:][::-1]    
+        if first_half == second_half:
+            return [first_half]
+        else:
+            return None
+
+    def __str__(self):
+        return "deduplicate_reversed"
+
+DEDUPLICATE_REVERSED = DeduplicateReversed() 
