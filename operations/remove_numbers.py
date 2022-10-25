a8dc5541b675fc04d90fdba3e6a876e3c38356d2
Michael Eichberg <michael.eichberg@bka.bund.de>
2022-10-25 09:21:29 +0200

    improved coding conventions

--- operations/remove_numbers.py
+++ operations/remove_numbers.py
@@ -7,14 +7,14 @@ from operations.operation import Operation
 class RemoveNumbers(Operation):
     """Removes all numbers from an entry."""
 
-    re_no_numbers = re.compile("[^0-9]+")
+    _re_no_numbers = re.compile("[^0-9]+")
 
     def is_transformer(self) -> bool: return True
 
     def process(self, entry: str) -> List[str]:
         entries = [
             i.group(0) 
-            for i in RemoveNumbers.re_no_numbers.finditer(entry)
+            for i in RemoveNumbers._re_no_numbers.finditer(entry)
         ]
         if len(entries) == 0:
             return []
