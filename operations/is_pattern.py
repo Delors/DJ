4c2392ae174745420518dad7cf78befc1a966062
Michael Eichberg <michael.eichberg@bka.bund.de>
2023-02-07 16:46:21 +0100

    added support for identifying patterns of three letters

--- operations/is_pattern.py
+++ operations/is_pattern.py
@@ -4,7 +4,10 @@ from dj_ast import Filter
 
 
 class IsPattern(Filter):
-    """ Identifies repetitions of a single character or a pair of characters.
+    """ Identifies repetitions of 
+         - a single character 
+         - a pair of characters
+         - three characters
         E.g., "aaaaa" or "qpqpqp" are identified as patterns.
     """
 
@@ -12,20 +15,31 @@ class IsPattern(Filter):
 
     def process(self, entry: str) -> List[str]:
         length = len(entry)
-        if length <= 2:
-            return None
+        if length < 2:
+            return []
 
         first_char = entry[0]
         if all(first_char == c for c in entry[1:]):
             return [entry]
 
         second_char = entry[1]
-        if first_char == second_char or length % 2 != 0 or length == 2:
+        if first_char == second_char or length == 2:
             # well... given the previous test it is obviously not a pattern
             # that is based on the repetition of two different characters
             return []
 
-        if entry == entry[0:2]*(length//2):
+        if length % 2 == 0 and entry == entry[0:2]*(length//2):
+            return [entry]
+
+        if length < 6 or length % 3 != 0:
+            return []
+        third_char = entry[2]
+        if (
+                third_char != first_char or
+                third_char != second_char or
+                second_char != first_char
+            ) and \
+                entry == entry[0:3]*(length//3):
             return [entry]
         else:
             return []
