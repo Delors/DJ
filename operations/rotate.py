e181ffff612afffb82b5398e66c0558e303d5f3e
Michael Eichberg <michael.eichberg@bka.bund.de>
2023-02-07 16:39:58 +0100

    rotate now takes a parameter to determine the rotation length

--- operations/rotate.py
+++ operations/rotate.py
@@ -4,20 +4,26 @@ from dj_ast import Transformer
 
 
 class Rotate(Transformer):
-    """Rotates the entry; e.g., "abc" is transformed to "cba"."""
+    """ Rotates the entry n chars to the left; e.g., `rotate 1`
+        transforms "abc" to "bca".
+
+        (If you want to get the reverse, i.e., "cba" use
+        the reverse operation.)
+    """
+    def __init__(self,rotate_by: int) -> None:
+        self.rotate_by = rotate_by
 
     def op_name() -> str: return "rotate"
 
     def process(self, entry: str) -> List[str]:
-        r = []
-        for i in entry:
-            r = [i] + r
-        rotated_entry = "".join(r)
+        
+        rotated_entry = entry[self.rotate_by:] + entry[0:self.rotate_by]
 
         if rotated_entry != entry:
             return [rotated_entry]
         else:
             return None
 
+    def __str__(self):
+        return f"{Rotate.op_name()} {self.rotate_by}"
 
-ROTATE = Rotate()
