a49107db442232b95341783a25cc07c47693b21d
Michael Eichberg <michael.eichberg@bka.bund.de>
2023-02-28 10:57:36 +0100

    fixed test

--- operations/correct_spelling_test.py
+++ operations/correct_spelling_test.py
@@ -1,52 +1,55 @@
 import unittest
 
-from operations.correct_spelling import CORRECT_SPELLING as CS
+from operations.correct_spelling import CorrectSpelling
 
 
 class TestCorrectSpelling(unittest.TestCase):
 
-    def test_is_transformer(self):
-        self.assertTrue(CS.is_transformer())
+    def setUp(self):
+        self.CS = CorrectSpelling().init(None,None)
+
+    def test_is_transformer(self):        
+        self.assertTrue(self.CS.is_transformer())
 
     def test__str__(self):
-        self.assertEqual(CS.__str__(), "correct_spelling")
+        self.assertEqual(self.CS.__str__(), "correct_spelling")
 
     def test_correct_word(self):
-        self.assertEqual(CS.process("ape"), [])
+        self.assertEqual(self.CS.process("ape"), [])
 
     def test_wrong_capitalization(self):
-        self.assertEqual(CS.process("munich"), ["Munich"])
+        self.assertEqual(self.CS.process("munich"), ["Munich"])
 
     def test_multiple_corrections(self):
-        suggestions = CS.process("Houres")
+        suggestions = self.CS.process("Houres")
         self.assertIn("Hours", suggestions)
         self.assertIn("Houses", suggestions)
 
     def test_use_of_damerau_levenshtein_is_default(self):
-        self.assertTrue(CS.USE_DAMERAU_LEVENSHTEIN)
+        self.assertTrue(self.CS.USE_DAMERAU_LEVENSHTEIN)
 
     def test_use_of_damerau_levenshtein_corrections(self):
-        suggestions = CS.process("Tset")
+        suggestions = self.CS.process("Tset")
         self.assertIn("Test", suggestions)
 
     def test_handling_of_capitalization(self):
-        suggestions = CS.process("TeSt")
+        suggestions = self.CS.process("TeSt")
         self.assertListEqual(["Test"], suggestions)
 
-        suggestions = CS.process("hallenbad")
+        suggestions = self.CS.process("hallenbad")
         self.assertListEqual(["Hallenbad"], suggestions)
 
-        suggestions = CS.process("HaLlenBad")
+        suggestions = self.CS.process("HaLlenBad")
         self.assertListEqual(["Hallenbad"], suggestions)
 
     def test_arbitrary_chars(self):
-        suggestions = CS.process("MwDkadfuaSSeeeadfas")
+        suggestions = self.CS.process("MwDkadfuaSSeeeadfas")
         self.assertIsNone(suggestions)
 
     # ##########################################################################
     # For the next tests, we change the distance calculator to Levenshtein:
 
     def test_use_of_levenshtein_corrections(self):
-        CS.USE_DAMERAU_LEVENSHTEIN = False
-        suggestions = CS.process("Tset")
+        self.CS.USE_DAMERAU_LEVENSHTEIN = False
+        suggestions = self.CS.process("Tset")
         self.assertNotIn("Test", suggestions)
